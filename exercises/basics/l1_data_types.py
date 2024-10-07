b41cd8ec904e4491bf23242d0d2755222ef51830
Christian G <flamerattacker7@gmail.com>
2024-10-07 16:20:30 -0400

    Day 1 workshop

---
+++ exercises/basics/l1_data_types.py
@@ -0,0 +1,11 @@
+birds = 2
+object = "stone"
+
+# Edit between these lines to make the test pass
+
+formatted_string = "?? birds with 1 ??!" # format this string!
+
+# Edit between these lines to make the test pass
+
+def test_name():
+    assert formatted_string == "2 birds with 1 stone!"
